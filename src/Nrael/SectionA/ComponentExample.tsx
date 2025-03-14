2581fa0130c26f765137048cb56775018680d880
S <satria.pandega@gmail.com>
2025-03-14 23:29:45 +0700

    Component Example

---
+++ src/Nrael/SectionA/ComponentExample.tsx
@@ -0,0 +1,13 @@
+import "./ComponentExample.css";
+
+function ComponentExample() {
+  return (
+    <>
+      <div className="component-example">
+        <p>Hi this is a Component!</p>
+      </div>
+    </>
+  );
+}
+
+export default ComponentExample;
